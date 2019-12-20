094e5a37ff21a5f23c11b282dfcb3828a6da108c
cyc3086 <36428206+cyc3086@users.noreply.github.com>
2019-12-19 19:59:05 -0600

    Made it work

--- controller/bot.py
+++ controller/bot.py
@@ -1,13 +1,39 @@
 import twitter, os, json
+import re
+import random
+from flask import Flask
 
-config_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "config.json")
+class twitter_bot:
+    def __init__(self, config_file="config.json"):
+        self.config_path = config_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), config_file)
+        # self.config=None
+        with open(self.config_path) as f:
+            self.config = json.load(f)
+        self.api = twitter.Api(
+            consumer_key=self.config["consumer_key"], 
+            consumer_secret=self.config["consumer_secret"], 
+            access_token_key=self.config["access_token_key"], 
+            access_token_secret=self.config["access_token_secret"]
+        )
+    def getTimeline(self,screen_name='potus'):
+        user = self.api.GetUser(screen_name = screen_name)
+        return [s.text for s in self.api.GetUserTimeline(user.id)]
+    def postTweet(self,content):
+        status = self.api.PostUpdate(content)
+        return status
+    def isVowel(self,t):
+        return t.lower() == 'a' or t == 'e' or t == 'i' or t == 'o' or t == 'u'
+    def owoify(self,text):
+        faces = ['OwO', 'UwU','>w<','^w^','-w-',';; w ;;']
+        for c in range(len(text)-1):
+            if(text[c].lower() == 'r' and isVowel(text[c+1])):
+                text = text[:c] + 'w' + text[c+1:]
+            elif((isVowel(text[c]) and text[c+1].lower() == 'r')):
+                text = text[:c+1] + 'w' + text[c+2:]
+            elif(text[c].lower() == 'n' and isVowel(text[c+1])):
+                text = text[:c] + 'ny' + text[c+1:]
+            elif(text[c].lower() == 'l'):
+                text = text[:c] + 'w' + text[c+1:]
+        return re.sub('!',' ' + faces[random.randint(0,len(faces)-1)],text) + ' nya~'
 
-with open(config_path) as f:
-    config = json.load(f)
-
-api = twitter.Api(
-    consumer_key=config["consumer_key"], 
-    consumer_secret=config["consumer_secret"], 
-    access_token_key=config["access_token_key"], 
-    access_token_secret=config["access_token_secret"]
-)
+tb = twitter_bot()
--- controller/routes.py
+++ controller/routes.py
@@ -1,6 +1,15 @@
-from controller import app
-from flask import render_template
+from controller import app,bot
+from flask import render_template,redirect
+
+import random
+
+b = bot.twitter_bot()
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html',title='CWAD TWITTER BOT')
+
+@app.route('/tweet')
+def tweet():
+    b.postTweet('This is a test %s' % random.randint(0,69))
+    return redirect('/',code=302)
