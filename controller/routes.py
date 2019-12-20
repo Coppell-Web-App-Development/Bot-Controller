f0178e43ef427556f455532e20a73490b59ff9a2
cyc3086 <36428206+cyc3086@users.noreply.github.com>
2019-12-20 13:35:55 -0600

    updated stuff, now accepts post requests

--- controller/routes.py
+++ controller/routes.py
@@ -1,15 +1,15 @@
 from controller import app,bot
-from flask import render_template,redirect
+from flask import render_template,redirect,request
 
 import random
 
 b = bot.twitter_bot()
 
-@app.route('/')
+@app.route('/',methods=['GET','POST'])
 def index():
     return render_template('index.html',title='CWAD TWITTER BOT')
 
-@app.route('/tweet')
+@app.route('/tweet', methods=['GET','POST'])
 def tweet():
-    b.postTweet('This is a test %s' % random.randint(0,69))
+    b.postTweet(request.form['content'])
     return redirect('/',code=302)
