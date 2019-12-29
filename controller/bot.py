4979ca44226178363eb76960b52838b019cb20d2
unknown <ragibarnab@gmail.com>
2019-12-28 19:41:50 -0600

    done with example

--- controller/bot.py
+++ controller/bot.py
@@ -1,5 +1,10 @@
 import twitter, os, json
 
+
+# looks for config.json file for api credentials at root dir of project
+# api credentials are on developer.twitter.com
+# sign in as cwadbot and go under Apps > Bot Controller
+# create config.json and store the credentials from "keys and tokens"
 config_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "config.json")
 
 with open(config_path) as f:
@@ -11,3 +16,6 @@ api = twitter.Api(
     access_token_key=config["access_token_key"], 
     access_token_secret=config["access_token_secret"]
 )
+
+def post_message(msg):
+    return api.PostUpdate(status=msg)
--- controller/routes.py
+++ controller/routes.py
@@ -1,6 +1,20 @@
 from controller import app
-from flask import render_template
+from flask import render_template, request, jsonify
+import controller.bot
 
-@app.route('/')
+
+@app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+    if request.method == 'GET':
+        return render_template('index.html')
+
+@app.route('/post_message', methods=['POST'])
+def post_message():
+    if request.method == 'POST':
+        message = request.form['message']
+        try:
+            twitter_status = controller.bot.post_message(msg=message)
+            text = twitter_status.__dict__['text']
+        except:
+            return render_template('index.html', error=True)
+        return render_template('index.html', success=True, msg=text)
